91809d589b2b8aaf2ee0e0db1801f3eb5dcc70e0
davisethan <davietha@oregonstate.edu>
2021-12-28 15:15:54 -0800

    Function headers added

--- localPassport.ts
+++ localPassport.ts
@@ -3,11 +3,21 @@ import * as bcrypt from "bcrypt";
 class LocalPassport {
     private memoryStore: any;
 
+    /**
+     * Local authentication signup and login
+     * @param memoryStore Username and password pair memorystore
+     */
     public constructor(memoryStore: any) {
         this.memoryStore = memoryStore;
     }
 
-    public localSignupStrategy: any = async (username: string, password: string, done: any): Promise<any> => {
+    /**
+     * Local signup
+     * @param username Local signup username
+     * @param password Local signup password
+     * @param done Local signup finished callback
+     */
+    public localSignupStrategy: any = (username: string, password: string, done: any): void => {
         if (this.memoryStoreHasUsername(username)) {
             done(null, null);
         } else {
@@ -20,7 +30,13 @@ class LocalPassport {
         }
     }
 
-    public localLoginStrategy: any = async (username: string, password: string, done: any): Promise<any> => {
+    /**
+     * Local login
+     * @param username Local login username
+     * @param password Local login password
+     * @param done Local login finished callback
+     */
+    public localLoginStrategy: any = (username: string, password: string, done: any): void => {
         if (!this.memoryStoreHasUsername(username)) {
             done(null, null);
         } else if (!this.memoryStorePasswordMatch(username, password)) {
@@ -34,10 +50,20 @@ class LocalPassport {
         }
     }
 
+    /**
+     * Authentication serialize user for user session
+     * @param user User for user session
+     * @param done Authentication serialization finished callback
+     */
     public serializeUser: any = (user: any, done: any): void => {
         done(null, user.username);
     }
 
+    /**
+     * Authentication deserialize user for user session
+     * @param username Username for user
+     * @param done Authentication deserialization finished callback
+     */
     public deserializeUser: any = (username: string, done: any): void => {
         const user: any = {
             username: username,
@@ -46,20 +72,41 @@ class LocalPassport {
         done(null, user);
     }
 
+    /**
+     * Memorystore has username
+     * @param username Username maybe in memorystore
+     * @returns Memorystore has username
+     */
     private memoryStoreHasUsername: any = (username: string): boolean => {
         return this.memoryStore.store.has(username);
     }
 
+    /**
+     * Memorystore set username and password hash pair
+     * @param username Username in pair
+     * @param password Password in pair
+     */
     private memoryStoreSetUsernameAndPassword: any = async (username: string, password: string): Promise<any> => {
         const saltRounds: number = 8;
         const hash: string = await bcrypt.hash(password, saltRounds);
         this.memoryStore.store.set(username, hash);
     }
 
+    /**
+     * Memorystore get password hash
+     * @param username Username in memorystore pair
+     * @returns Password hash
+     */
     private memoryStoreGetPassword: any = (username: string): string => {
         return this.memoryStore.store.get(username);
     }
 
+    /**
+     * Memorystore password hash matches password
+     * @param username Username in pair
+     * @param password Password maybe in pair
+     * @returns Password hash matches password
+     */
     private memoryStorePasswordMatch: any = async (username: string, password: string): Promise<any> => {
         const hash: string = this.memoryStoreGetPassword(username);
         const passwordMatch: boolean = await bcrypt.compare(password, hash);
--- router.ts
+++ router.ts
@@ -5,67 +5,87 @@ class Router {
     private passport: any;
     private router: any;
 
+    /**
+     * Webapp router
+     * @param passport Authentication module
+     */
     public constructor(passport: any) {
         this.passport = passport;
         this.router = express.Router();
         this.setRouter();
     }
 
+    /**
+     * Get webapp router
+     * @returns Webapp router
+     */
     public getRouter: any = (): any => {
         return this.router;
     }
 
+    /**
+     * Set webapp router
+     */
     private setRouter: any = (): void => {
+        // Home page
         this.router.get("/", (req: any, res: any): void => {
-            const filename: string = "home.html";
-            const options: any = {
-                root: path.join(__dirname, "/public")
-            };
-            res.sendFile(filename, options);
+            res.sendFile(path.join(__dirname, "/public/home.html"));
         });
 
+        // Signup page
         this.router.get("/signup", (req: any, res: any): void => {
-            const filename: string = "signup.html";
-            const options: any = {
-                root: path.join(__dirname, "/public")
-            };
-            res.sendFile(filename, options);
+            res.sendFile(path.join(__dirname, "/public/signup.html"));
         });
 
+        // Login page
         this.router.get("/login", (req: any, res: any): void => {
-            const filename: string = "login.html";
-            const options: any = {
-                root: path.join(__dirname, "/public")
-            };
-            res.sendFile(filename, options);
+            res.sendFile(path.join(__dirname, "/public/login.html"));
         });
 
+        // User page
         this.router.get("/user/:username", this.loggedIn, (req: any, res: any): void => {
-            const filename: string = "user.html";
-            const options: any = {
-                root: path.join(__dirname, "/public")
-            };
-            res.sendFile(filename, options);
+            res.sendFile(path.join(__dirname, "/public/user.html"));
         });
 
+        // Logout post
         this.router.post("/logout", this.loggedIn, (req: any, res: any): void => {
             req.logout();
             res.redirect("/");
         });
 
+        // Signup post
         this.router.post("/local-signup", this.passport.authenticate("local-signup", {
             failureRedirect: "/signup"
         }), (req: any, res: any): void => {
             res.redirect(`/user/${req.user.username}`);
         });
 
+        // Login post
         this.router.post("/local-login", this.passport.authenticate("local-login", {
             failureRedirect: "/login"
         }), (req: any, res: any): void => {
             res.redirect(`/user/${req.user.username}`);
         });
+
+        // 404 Page Not Found
+        this.router.use((req: any, res: any, next: any): void => {
+            res.status(404);
+            res.sendFile(path.join(__dirname, "/public/404.html"));
+        });
+
+        // 500 Server Error
+        this.router.use((err: any, req: any, res: any, next: any): void => {
+            res.status(500);
+            res.sendFile(path.join(__dirname, "/public/500.html"));
+        });
     }
 
+    /**
+     * User is logged in middleware
+     * @param req User request
+     * @param res User response
+     * @param next Next middleware
+     */
     private loggedIn: any = (req: any, res: any, next: any): void => {
         req.user ? next() : res.redirect("/login");
     }
